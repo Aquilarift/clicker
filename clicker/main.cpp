9a3a149fda83cabb35a1571babcc2f6972e1df84
Aquilarift <kelatyt@gmail.com>
2020-12-26 03:20:28 +0300

    redone

--- clicker/main.cpp
+++ clicker/main.cpp
@@ -4,34 +4,33 @@
 
 using namespace std;
 
-
 bool oofs, clState = true;
 int slep, x, y;
 
 void check() {
-	while (clState) {
-		if (GetAsyncKeyState(VK_MULTIPLY)) {
-			clState = false;
-		}
+	while (true) {
+		if (GetAsyncKeyState(VK_DIVIDE)) exit(0);
+		if (GetAsyncKeyState(VK_MULTIPLY) & 0x0001) clState = !clState;
 	}
 }
 
 int click() {
-	POINT* pos = new POINT();
-	if (!GetCursorPos(pos)) { delete pos; return 1; }
+	POINT pos;
+	if (!GetCursorPos(&pos)) return 1;
 
-	x = pos->x;
-	y = pos->y;
+	x = pos.x;
+	y = pos.y;;
 
-	while (clState) {
-		if (oofs) SetCursorPos(x, y);
-		mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
-		mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+	while (true) {
+		if (clState) {
+			if (oofs) SetCursorPos(x, y);
+			mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+			mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
 
-		Sleep(slep);
+			Sleep(slep);
+		}
 	}
 
-	delete pos;
 	return 0;
 }
 
